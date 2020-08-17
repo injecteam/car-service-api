c63fd88e459c99bd52a9232b5e8e5877cd7e84b8
nikolasmelui <slak@samaradom.ru>
2020-08-17 23:03:00 +0400

    Update create method in the UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,18 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
-import { Observable, from } from 'rxjs';
+import { Observable, from, throwError } from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
 import { User } from './user.interface';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    private readonly authService: AuthService,
   ) {}
 
   create(user: User): Observable<User> {
-    return from(this.userRepository.save(user));
+    return this.authService.hashPassword(user.password).pipe(
+      switchMap((hashedPassword: string) => {
+        const newUser = new UserEntity();
+        newUser.email = user.email;
+        newUser.password = hashedPassword;
+
+        return from(this.userRepository.save(newUser)).pipe(
+          map((user: User) => {
+            const { password, ...result } = user;
+            return result;
+          }),
+
+          catchError(error => throwError(error)),
+        );
+      }),
+    );
   }
 
   findOne(id: number): Observable<User> {
