6659684535011b8ec218e3b81eac0a17f63d0773
nikolasmelui <slak@samaradom.ru>
2020-08-17 12:57:01 +0400

    Add create method to the UserController (observable)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Observable } from 'rxjs';
+import { User } from './user.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Observable<User> {
+    return this.userService.create(user);
+  }
 }
