a695c292c5088d2df771643fb36321164bd36535
nikolasmelui <slak@samaradom.ru>
2020-08-17 21:48:26 +0400

    Add generateJWT method to the AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { generate, Observable, from } from 'rxjs';
+import { User } from 'src/user/user.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  generateJWT(user: User): Observable<string> {
+    return from(this.jwtService.signAsync({ user }));
+  }
+}
