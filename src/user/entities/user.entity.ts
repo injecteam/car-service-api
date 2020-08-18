88c278acc45c12fb0b2529dc41a8ae90bc495ce6
nikolasmelui <slak@samaradom.ru>
2020-08-19 02:13:18 +0400

    Move out User entity to the standalone dir

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,12 +1,20 @@
 import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
 
-@Entity()
-export class UserEntity {
+@Entity('users')
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
   @Column()
+  name: string;
+
+  @Column()
+  surname: string;
+
+  @Column({ nullable: true })
+  age?: number;
+
+  @Column({ unique: true })
   email: string;
 
   @Column()
