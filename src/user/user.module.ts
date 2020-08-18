6161d2b6c83f8dd136be5ede524cac72a8756e86
nikolasmelui <slak@samaradom.ru>
2020-08-18 11:29:42 +0400

    Export UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,5 +9,6 @@ import { AuthModule } from 'src/auth/auth.module';
   imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
   providers: [UserService],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
