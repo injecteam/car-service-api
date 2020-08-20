b83a1c82439932ee5c4d01306f5fb8f4cdec8fa1
nikolasmelui <slak@samaradom.ru>
2020-08-20 14:23:26 +0400

    Delete useless import of UserService from AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,12 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
-import { RolesGuard } from '../common/guards/roles.guard';
 import { JwtAuthGuard } from './jwtauth.guard';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    forwardRef(() => UserModule),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -21,7 +18,7 @@ import { JwtStrategy } from './jwt.strategy';
       }),
     }),
   ],
-  providers: [AuthService, RolesGuard, JwtAuthGuard, JwtStrategy],
+  providers: [AuthService, JwtAuthGuard, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
