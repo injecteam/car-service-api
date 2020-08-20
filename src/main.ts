7971e830e346536d88c6acb5dc03b3aa9f8d42c9
nikolasmelui <slak@samaradom.ru>
2020-08-20 14:22:47 +0400

    Add global prefix 'api' to the REST endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  // FIXME: Check the in-method pipe validation and delete this one
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //   }),
+  // );
+
+  await app.listen(process.env.APPLICATION_PORT || 3000);
 }
 bootstrap();
