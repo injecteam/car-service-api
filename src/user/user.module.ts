61f8a71ba53c8880f9ba1bc5689f886ed9e7b944
nikolasmelui <slak@samaradom.ru>
2020-08-20 23:20:42 +0400

    Update UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
-import { User } from './entities/user.entity';
+import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
