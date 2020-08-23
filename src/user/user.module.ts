4b9c4ad2a5b9505914db406bcf9fb283bc94bc65
nikolasmelui <slak@samaradom.ru>
2020-08-24 01:42:21 +0400

    Import new (renamed) AuthenticatoinModule and Authorizatoin module into the UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthenticationModule } from 'src/authentication/authentication.module';
 import { User } from './user.entity';
+import { AuthorizationModule } from 'src/authorization/authorization.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    AuthenticationModule,
+    AuthorizationModule,
+  ],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
