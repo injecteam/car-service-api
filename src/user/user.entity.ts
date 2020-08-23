a22a68ab773aece69eba8ba2b4eff9419bf305a3
nikolasmelui <slak@samaradom.ru>
2020-08-24 01:39:44 +0400

    Update User entity Add role (AuthorizationRole) column to the User entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,40 +1,49 @@
 import { Entity, Column, BeforeInsert } from 'typeorm';
 import { AbstractEntity } from 'src/common/entities/abstract.entity';
-import { IsString, MinLength, MaxLength, IsNumber } from 'class-validator';
-import { Exclude, classToPlain } from 'class-transformer';
+import { AuthorizationRole } from 'src/authorization/authorization-role.enum';
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  IsNumber,
+  IsEmail,
+} from 'class-validator';
 
 @Entity('users')
 export class User extends AbstractEntity {
-  @Column()
+  @Column({ type: 'varchar', length: 30, nullable: false })
   @IsString()
   @MinLength(3)
   @MaxLength(30)
   name: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 30, nullable: false })
   @IsString()
   @MinLength(3)
   @MaxLength(30)
   surname: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'smallint', nullable: true })
   @IsNumber()
   age?: number;
 
-  @Column({ unique: true })
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  @IsEmail()
   email: string;
+
+  @Column({
+    type: 'enum',
+    enum: AuthorizationRole,
+    default: AuthorizationRole.USER,
+  })
+  role: AuthorizationRole;
 
   @Column()
   @IsString()
-  @Exclude()
   password: string;
 
   @BeforeInsert()
   emailToLowerCase(): void {
     this.email = this.email.toLowerCase();
-  }
-
-  toJSON(): Record<string, any> {
-    return classToPlain(this);
   }
 }
