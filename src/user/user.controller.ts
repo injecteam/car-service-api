94fc8400a26a000ea99819c36555283435c9e7ce
nikolasmelui <slak@samaradom.ru>
2020-08-25 23:36:10 +0400

    Add new updateRole endpoint to the UserController Add ClassSerializerInterceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,8 @@ import {
   ValidationPipe,
   UseGuards,
   Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthenticationJwtGuard } from 'src/authentication/jwtauth.guard';
@@ -21,18 +23,21 @@ import {
   UpdateResponseDTO,
   SignInRequestDTO,
   SignInResponseDTO,
+  UpdateRoleRequestDTO,
+  UpdateRoleResponseDTO,
+  FindAllResponseDTO,
 } from './user.dto';
 import { Roles } from 'src/authorization/roles.decorator';
 import { OwnOrRoleGuard } from 'src/authorization/own-or-role.guard';
 import { RoleGuard } from 'src/authorization/role.guard';
 import { AuthorizationRole } from 'src/authorization/authorization-role.enum';
-// import { RolesGuard } from 'src/roles/roles.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('signup')
+  @UseInterceptors(ClassSerializerInterceptor)
   signUp(
     @Body(new ValidationPipe({ whitelist: true }))
     signUpRequestDTO: SignUpRequestDTO,
@@ -42,7 +47,8 @@ export class UserController {
 
   @Post('signin')
   signIn(
-    @Body() signInRequestDTO: SignInRequestDTO,
+    @Body(new ValidationPipe({ whitelist: true }))
+    signInRequestDTO: SignInRequestDTO,
   ): Promise<SignInResponseDTO> {
     return this.userService.signIn(signInRequestDTO);
   }
@@ -50,23 +56,27 @@ export class UserController {
   @Get('findbyemail')
   @Roles(AuthorizationRole.ADMIN)
   @UseGuards(AuthenticationJwtGuard, RoleGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
   findByEmail(@Query('email') email: string): Promise<FindByEmailResponseDTO> {
     return this.userService.findByEmail(email);
   }
 
   @Get(':id')
+  @UseInterceptors(ClassSerializerInterceptor)
   findById(@Param('id') id: number): Promise<FindByIdResponseDTO> {
     return this.userService.findById(id);
   }
 
   @Get()
-  findAll(): Promise<FindByIdResponseDTO[]> {
+  @UseInterceptors(ClassSerializerInterceptor)
+  findAll(): Promise<FindAllResponseDTO[]> {
     return this.userService.findAll();
   }
 
   @Put(':id')
   @Roles(AuthorizationRole.ADMIN)
   @UseGuards(AuthenticationJwtGuard, OwnOrRoleGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
   update(
     @Param('id') id: number,
     @Body(new ValidationPipe({ whitelist: true }))
@@ -80,5 +90,17 @@ export class UserController {
   @UseGuards(AuthenticationJwtGuard, RoleGuard)
   deleteOne(@Param('id') id: number): Promise<number> {
     return this.userService.delete(id);
+  }
+
+  @Put(':id/role')
+  @Roles(AuthorizationRole.SUPER)
+  @UseGuards(AuthenticationJwtGuard, RoleGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  updateRole(
+    @Param('id') id: number,
+    @Body(new ValidationPipe({ whitelist: true }))
+    updateRoleRequestDTO: UpdateRoleRequestDTO,
+  ): Promise<UpdateRoleResponseDTO> {
+    return this.userService.updateRole(id, updateRoleRequestDTO);
   }
 }
