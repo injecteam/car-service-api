acae74f5b49bc99ab5e0d262db800c8ddffd274c
nikolasmelui <slak@samaradom.ru>
2020-08-26 10:15:22 +0400

    Set up the swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Car Service API')
+    .setDescription('An API for the car service')
+    .setVersion('0.3.0')
+    .addTag('car_service_api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.APPLICATION_PORT || 3000);
 }
 bootstrap();
