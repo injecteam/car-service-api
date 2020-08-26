a4ce1cc924ffdeb34f32ee470b336afcf5a03ca3
nikolasmelui <slak@samaradom.ru>
2020-08-26 15:48:20 +0400

    Update AuthenticationService (cleanup imports)

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
 import { UserJwtPayload } from './authentication.type';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthenticationService {
