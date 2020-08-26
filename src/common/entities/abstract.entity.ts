423d1a7de4a591fff32f645f735a7a5294bf24a7
nikolasmelui <slak@samaradom.ru>
2020-08-26 15:47:48 +0400

    Update AbstractEntity with swagger TODO comments

--- src/common/entities/abstract.entity.ts
+++ src/common/entities/abstract.entity.ts
@@ -1,16 +1,19 @@
 import {
-  PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 
 export abstract class AbstractEntity {
   @PrimaryGeneratedColumn('increment')
+  // TODO: Describe API property (swagger stuff)
   id: number;
 
   @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  // TODO: Describe API property (swagger stuff)
   createdAt: Date;
 
   @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  // TODO: Describe API property (swagger stuff)
   updatedAt: Date;
 }
