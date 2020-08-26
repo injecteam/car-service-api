15ab4902f306b7f7aa67eebff924d799f2cd627c
nikolasmelui <slak@samaradom.ru>
2020-08-26 10:03:02 +0400

    Remove unnecessary (fixed) comments in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-
-  // FIXME: Check the in-method pipe validation and delete this one
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //   }),
-  // );
-
   await app.listen(process.env.APPLICATION_PORT || 3000);
 }
 bootstrap();
