404c74eefa5d7c734b6c385b466c15b4128bdbb8
nikolasmelui <slak@samaradom.ru>
2020-08-26 15:50:41 +0400

    Update UserService (a lot has been done)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,30 @@
+// Packages
 import {
   Injectable,
   ConflictException,
-  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DeleteResult, UpdateResult } from 'typeorm';
+// Services
+import { AuthenticationService } from 'src/authentication/authentication.service';
+// Entities
+import { User } from './user.entity';
+// Types
+import { UserJwtPayload } from 'src/authentication/authentication.type';
+// DTOs
+import { SignUpRequestDTO, SignUpResponseDTO } from './dto/signup.dto';
+import { SignInRequestDTO, SignInResponseDTO } from './dto/signin.dto';
+import { FindAllResponseDTO } from './dto/find-all.dto';
+import { FindByIdResponseDTO } from './dto/find-by-id.dto';
+import { FindByEmailResponseDTO } from './dto/find-by-email.dto';
+import { UpdateRequestDTO, UpdateResponseDTO } from './dto/update.dto';
 import {
-  SignUpRequestDTO,
-  SignUpResponseDTO,
-  FindByIdResponseDTO,
-  FindByEmailResponseDTO,
-  UpdateResponseDTO,
-  SignInRequestDTO,
-  SignInResponseDTO,
-  FindAllResponseDTO,
   UpdateRoleRequestDTO,
   UpdateRoleResponseDTO,
-} from './user.dto';
-import { AuthenticationService } from 'src/authentication/authentication.service';
-import { User } from './user.entity';
-import { Repository, DeleteResult } from 'typeorm';
-import { UpdateRequestDTO } from './user.dto';
-import { UserJwtPayload } from 'src/authentication/authentication.type';
+} from './dto/update-role.dto';
 
 @Injectable()
 export class UserService {
@@ -41,10 +43,11 @@ export class UserService {
       );
       user.password = hashedPassword;
       await this.userRepository.save(user);
+      delete user.role;
       return user;
     } catch (error) {
       if (error.code === '23505')
-        throw new ConflictException('User with this email already exists');
+        throw new ConflictException('User with this email already exists.');
       throw new InternalServerErrorException();
     }
   }
@@ -55,13 +58,13 @@ export class UserService {
       const user: User = await this.userRepository.findOne({
         email,
       });
-      if (!user) throw new UnauthorizedException('Invalid credentials');
+      if (!user) throw new UnauthorizedException('Invalid credentials.');
       const isValidPassword: boolean = await this.authenticationService.comparePasswords(
         password,
         user.password,
       );
       if (!isValidPassword)
-        throw new UnauthorizedException('Invalid credentials');
+        throw new UnauthorizedException('Invalid credentials.');
       const userJwtPayload: UserJwtPayload = {
         id: user.id,
         name: user.name,
@@ -74,7 +77,18 @@ export class UserService {
       );
       return { access_token: jwt };
     } catch (error) {
-      throw new UnauthorizedException('Invalid credentials');
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+  }
+
+  async findAll(): Promise<FindAllResponseDTO[]> {
+    try {
+      const users: FindAllResponseDTO[] = await this.userRepository.find();
+      if (!users.length) throw new InternalServerErrorException();
+      users.forEach(user => delete user.role);
+      return users;
+    } catch (error) {
+      throw new InternalServerErrorException();
     }
   }
 
@@ -103,24 +117,18 @@ export class UserService {
     }
   }
 
-  async findAll(): Promise<FindAllResponseDTO[]> {
-    try {
-      const users: FindAllResponseDTO[] = await this.userRepository.find();
-      if (!users) throw new NotFoundException();
-      users.forEach(user => delete user.role);
-      return users;
-    } catch (error) {
-      if (error.code === 'P0002') throw new NotFoundException();
-      throw new InternalServerErrorException();
-    }
-  }
-
   async update(
     id: number,
     updateRequestDTO: UpdateRequestDTO,
   ): Promise<UpdateResponseDTO> {
     try {
-      await this.userRepository.update(id, updateRequestDTO);
+      console.log('heree');
+      const result: UpdateResult = await this.userRepository.update(
+        id,
+        updateRequestDTO,
+      );
+      const affected: number = result.affected;
+      if (affected === 0) throw new NotFoundException();
       const user: UpdateResponseDTO = await this.userRepository.findOne(id);
       return user;
     } catch (error) {
@@ -128,10 +136,12 @@ export class UserService {
     }
   }
 
+  // TODO: Make 'delete' not delete but just mark entry as deleted
   async delete(id: number): Promise<number> {
     try {
       const result: DeleteResult = await this.userRepository.delete(id);
-      const affected: number | null = result.affected;
+      const affected: number = result.affected;
+      if (affected === 0) throw new NotFoundException();
       return affected;
     } catch (error) {
       throw new InternalServerErrorException();
@@ -143,7 +153,12 @@ export class UserService {
     updateRoleRequestDTO: UpdateRoleRequestDTO,
   ): Promise<UpdateRoleResponseDTO> {
     try {
-      await this.userRepository.update(id, updateRoleRequestDTO);
+      const result: UpdateResult = await this.userRepository.update(
+        id,
+        updateRoleRequestDTO,
+      );
+      const affected: number = result.affected;
+      if (affected === 0) throw new NotFoundException();
       const user: UpdateRoleResponseDTO = await this.userRepository.findOne(id);
       return user;
     } catch (error) {
