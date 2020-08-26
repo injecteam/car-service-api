66a6121c339953b9f5f18fa6f34c8e14d893c65f
nikolasmelui <slak@samaradom.ru>
2020-08-26 15:51:07 +0400

    Update UserController Add swagger metadata all over the UserController controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,43 +1,65 @@
+// Packages
 import {
-  Controller,
   Post,
   Body,
-  Param,
   Get,
   Put,
-  Delete,
-  ValidationPipe,
-  UseGuards,
   Query,
+  Param,
+  Delete,
+  UseGuards,
+  Controller,
+  ValidationPipe,
   UseInterceptors,
   ClassSerializerInterceptor,
 } from '@nestjs/common';
-import { UserService } from './user.service';
-import { AuthenticationJwtGuard } from 'src/authentication/jwtauth.guard';
 import {
-  SignUpRequestDTO,
-  SignUpResponseDTO,
-  FindByIdResponseDTO,
-  FindByEmailResponseDTO,
-  UpdateRequestDTO,
-  UpdateResponseDTO,
-  SignInRequestDTO,
-  SignInResponseDTO,
+  ApiTags,
+  ApiBody,
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiBearerAuth,
+  ApiConflictResponse,
+  ApiInternalServerErrorResponse,
+  ApiUnauthorizedResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
+// Services
+import { UserService } from './user.service';
+// Guards
+import { RoleGuard } from 'src/authorization/role.guard';
+import { OwnOrRoleGuard } from 'src/authorization/own-or-role.guard';
+import { AuthenticationJwtGuard } from 'src/authentication/jwtauth.guard';
+// Decorators
+import { Roles } from 'src/authorization/roles.decorator';
+// Enums
+import { AuthorizationRole } from 'src/authorization/authorization-role.enum';
+// DTOs
+import { SignUpRequestDTO, SignUpResponseDTO } from './dto/signup.dto';
+import { SignInRequestDTO, SignInResponseDTO } from './dto/signin.dto';
+import { FindAllResponseDTO } from './dto/find-all.dto';
+import { FindByIdResponseDTO } from './dto/find-by-id.dto';
+import { FindByEmailResponseDTO } from './dto/find-by-email.dto';
+import { UpdateRequestDTO, UpdateResponseDTO } from './dto/update.dto';
+import {
   UpdateRoleRequestDTO,
   UpdateRoleResponseDTO,
-  FindAllResponseDTO,
-} from './user.dto';
-import { Roles } from 'src/authorization/roles.decorator';
-import { OwnOrRoleGuard } from 'src/authorization/own-or-role.guard';
-import { RoleGuard } from 'src/authorization/role.guard';
-import { AuthorizationRole } from 'src/authorization/authorization-role.enum';
+} from './dto/update-role.dto';
 
 @Controller('users')
+@ApiTags('User')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('signup')
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiBody({ type: SignUpRequestDTO })
+  @ApiCreatedResponse({
+    description: 'User has been successfully registered.',
+  })
+  @ApiConflictResponse({ description: 'User with this email already exists.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
+  // TODO: Describe API response content DTO (swagger stuff)
   signUp(
     @Body(new ValidationPipe({ whitelist: true }))
     signUpRequestDTO: SignUpRequestDTO,
@@ -46,6 +68,10 @@ export class UserController {
   }
 
   @Post('signin')
+  @ApiBody({ type: SignInRequestDTO })
+  @ApiOkResponse({ description: 'User has been successfully logged in.' })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials.' })
+  // TODO: Describe API response content DTO (swagger stuff)
   signIn(
     @Body(new ValidationPipe({ whitelist: true }))
     signInRequestDTO: SignInRequestDTO,
@@ -53,30 +79,47 @@ export class UserController {
     return this.userService.signIn(signInRequestDTO);
   }
 
-  @Get('findbyemail')
-  @Roles(AuthorizationRole.ADMIN)
-  @UseGuards(AuthenticationJwtGuard, RoleGuard)
-  @UseInterceptors(ClassSerializerInterceptor)
-  findByEmail(@Query('email') email: string): Promise<FindByEmailResponseDTO> {
-    return this.userService.findByEmail(email);
-  }
-
-  @Get(':id')
-  @UseInterceptors(ClassSerializerInterceptor)
-  findById(@Param('id') id: number): Promise<FindByIdResponseDTO> {
-    return this.userService.findById(id);
-  }
-
   @Get()
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOkResponse({ description: 'Users have been successfully returned.' })
+  @ApiInternalServerErrorResponse()
+  // TODO: Describe API response content DTO (swagger stuff)
   findAll(): Promise<FindAllResponseDTO[]> {
     return this.userService.findAll();
   }
 
+  @Get(':id')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOkResponse({ description: 'User has been successfully returned.' })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  // TODO: Describe API response content DTO (swagger stuff)
+  findById(@Param('id') id: number): Promise<FindByIdResponseDTO> {
+    return this.userService.findById(id);
+  }
+
+  @Get('findbyemail')
+  @Roles(AuthorizationRole.SUPER, AuthorizationRole.ADMIN)
+  @UseGuards(AuthenticationJwtGuard, RoleGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'User has been successfully returned.' })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  // TODO: Describe API response content DTO (swagger stuff)
+  findByEmail(@Query('email') email: string): Promise<FindByEmailResponseDTO> {
+    return this.userService.findByEmail(email);
+  }
+
   @Put(':id')
-  @Roles(AuthorizationRole.ADMIN)
+  @Roles(AuthorizationRole.SUPER, AuthorizationRole.ADMIN)
   @UseGuards(AuthenticationJwtGuard, OwnOrRoleGuard)
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'User has been successfully updated.' })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  // TODO: Describe API response content DTO (swagger stuff)
   update(
     @Param('id') id: number,
     @Body(new ValidationPipe({ whitelist: true }))
@@ -85,9 +128,15 @@ export class UserController {
     return this.userService.update(id, updateRequestDTO);
   }
 
+  // TODO: Make 'delete' not delete but just mark entry as deleted
   @Delete(':id')
-  @Roles(AuthorizationRole.ADMIN)
+  @Roles(AuthorizationRole.SUPER, AuthorizationRole.ADMIN)
   @UseGuards(AuthenticationJwtGuard, RoleGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'User has been successfully deleted.' })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  // TODO: Describe API response content DTO (swagger stuff)
   deleteOne(@Param('id') id: number): Promise<number> {
     return this.userService.delete(id);
   }
@@ -96,6 +145,11 @@ export class UserController {
   @Roles(AuthorizationRole.SUPER)
   @UseGuards(AuthenticationJwtGuard, RoleGuard)
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'User (role) has been successfully updated.' })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  // TODO: Describe API response content DTO (swagger stuff)
   updateRole(
     @Param('id') id: number,
     @Body(new ValidationPipe({ whitelist: true }))
