6e0df0e6ae2c17ba4fc8b59b1b28c28cb4852811
nikolasmelui <slak@samaradom.ru>
2020-08-26 15:45:00 +0400

    Move Update to the standalone update.dto.ts file

---
+++ src/user/dto/update.dto.ts
@@ -0,0 +1,46 @@
+import {
+  IsOptional,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsNumber,
+  Min,
+  Max,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../user.entity';
+
+export class UpdateRequestDTO {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  @ApiProperty({
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    description: 'name',
+  })
+  readonly name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  @ApiProperty({
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    description: 'surname',
+  })
+  readonly surname?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(18)
+  @Max(100)
+  @ApiProperty({ type: Number, minimum: 18, maximum: 100, description: 'age' })
+  readonly age?: number;
+}
+
+export class UpdateResponseDTO extends User {}
