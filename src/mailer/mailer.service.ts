6a9538cefa30a7064f96aca3ce15f853785d5d37
nikolasmelui <slak@samaradom.ru>
2020-08-28 08:59:07 +0400

    Create sendTestEmail method in the MailerService

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import * as nodemailer from 'nodemailer';
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  async sendTestEmail(): Promise<void> {
+    const transport = nodemailer.createTransport({
+      host: 'smtp.sendgrid.net',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_SERVICE_USER,
+        pass: process.env.EMAIL_SERVICE_PASSWORD,
+      },
+    });
+
+    const result = await transport.sendMail({
+      from: '"car_service_api" <foo@example.com>',
+      to: 'slak@samaradom.ru',
+      subject: 'Test email ✔',
+      html: '<b>Do u see mee?<b>',
+    });
+
+    console.info(result);
+  }
+}
