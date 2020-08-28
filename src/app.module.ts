f3e10b9ac8949eb65ffabad8e1e7664f3f6961bc
nikolasmelui <slak@samaradom.ru>
2020-08-28 08:44:31 +0400

    Inject MailerModule to the AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { AuthenticationModule } from './authentication/authentication.module';
     UserModule,
     AuthenticationModule,
     AuthorizationModule,
+    MailerModule,
   ],
   controllers: [],
   providers: [],
