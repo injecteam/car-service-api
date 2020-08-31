f27623c509e81a5d3494f2358a1067f29bedf3cb
nikolasmelui <slak@samaradom.ru>
2020-09-01 00:54:21 +0400

    Replace process.env inteercom with configService injection and its logic

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+import Mail from 'nodemailer/lib/mailer';
 @Injectable()
 export class MailerService {
-  async send(to: string, subject: string, html: string): Promise<void> {
-    const transport = nodemailer.createTransport({
-      host: process.env.NODE_MAILER_HOST,
-      port: Number(process.env.NODE_MAILER_PORT),
-      secure: Boolean(process.env.NODE_MAILER_SECURE),
+  private transport: Mail;
+  private transportEmail: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transport = nodemailer.createTransport({
+      host: this.configService.get<string>('NODE_MAILER_HOST'),
+      port: Number(this.configService.get<string>('NODE_MAILER_PORT')),
+      secure: Boolean(this.configService.get<string>('NODE_MAILER_SECURE')),
       auth: {
-        user: process.env.NODE_MAILER_USER,
-        pass: process.env.NODE_MAILER_PASSWORD,
+        user: this.configService.get<string>('NODE_MAILER_USER'),
+        pass: this.configService.get<string>('NODE_MAILER_PASSWORD'),
       },
     });
+    this.transportEmail = this.configService.get<string>('NODE_MAILER_EMAIL');
+  }
 
-    const result = await transport.sendMail({
-      from: `"car_service_api" <${process.env.NODE_MAILER_EMAIL}>`,
+  async send(to: string, subject: string, html: string): Promise<void> {
+    const result = await this.transport.sendMail({
+      from: `"car_service_api" <${this.transportEmail}>`,
       to,
       subject,
       html,
